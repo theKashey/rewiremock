29f1fd8bc2d2801878423e1e0e8196ee3bcb0cb6
Paweł Kapalla <pkapalla@xesenix.pl>
2018-03-05 03:38:17 +0100

    fix interceptor file path for windows

--- webpack/plugin.js
+++ webpack/plugin.js
@@ -1,7 +1,7 @@
 const {relative} = require("path");
 const {ConcatSource} = require("webpack-sources");
 
-const file = './' + relative(process.cwd(), __dirname + '/interceptor.js');
+const file = './' + relative(process.cwd(), __dirname + '/interceptor.js').replace(/\\/g, '/');
 
 const injectString = "/***/if(typeof __webpack_require__!=='undefined'){__webpack_require__ = __webpack_require__('" + file + "')(__webpack_require__, module);}\n";
 
@@ -9,7 +9,7 @@ class RewiremockPlugin {
   apply(compiler) {
     compiler.plugin('compilation', function (compilation) {
 
-      compilation.moduleTemplate.plugin("render", function (moduleSource) {
+      compilation.moduleTemplate.plugin('render', function (moduleSource) {
         const source = new ConcatSource();
         source.add(injectString);
         source.add(moduleSource);
