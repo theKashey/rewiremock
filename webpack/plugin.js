ac869fd957a59fda698e2fc900ebe865188399c9
Anton Korzunov <thekashey@gmail.com>
2018-08-03 23:09:28 +1000

    fix: relative path to webpack plugin

--- webpack/plugin.js
+++ webpack/plugin.js
@@ -1,7 +1,9 @@
 const {relative} = require("path");
 const {ConcatSource} = require("webpack-sources");
 
-const file = './' + relative(process.cwd(), __dirname + '/interceptor.js').replace(/\\/g, '/');
+const normalizePath = a => a[0] === '.' ? a : './' + a;
+
+const file = normalizePath(relative(process.cwd(), __dirname + '/interceptor.js').replace(/\\/g, '/'));
 
 const injectString = "/***/if(typeof __webpack_require__!=='undefined'){__webpack_require__ = __webpack_require__('" + file + "')(__webpack_require__, module);}\n";
 
